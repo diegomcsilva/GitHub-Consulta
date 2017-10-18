bc1a2d2d66165263652de5f9e6ebb887b6716d27
Diego Marcelo <diegomcsilva191@gmail.com>
2017-10-18 21:59:23 -0200

    Configurando o constructor

--- github-consulta/src/App.js
+++ github-consulta/src/App.js
@@ -3,31 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div id="main">
-            <div className="header">
-              <h1>Consulta de dados - GitHub</h1>
+
+    constructor() {
+        super();
+        this.state = {
+            lista: [{nome: 'Diego', login: 'diegomcsilva'}]
+        };
+    }
+
+    render() {
+        return (
+            <div id="main">
+                <div className="header">
+                    <h1>Consulta de dados - GitHub</h1>
+                </div>
+                <div className="content" id="content">
+                    <div className="content__data">
+                        <div className="content__data--block login">
+                            <label htmlFor="login">Login</label>
+                            <input id="login" type="text" name="login" value=""  />
+                        </div>
+                        <div className="content__data--block email">
+                            <label htmlFor="email">Email</label>
+                            <input id="email" type="email" name="email" value=""  />
+                        </div>
+                        <div className="content__data--button">
+                            <label></label>
+                            <button type="submit" className="content__data--button-search">Buscar</button>
+                        </div>
+                    </div>
+                    <div className="content__result">
+                        {
+                            this.state.lista.map(function(result){
+                                return (
+                                    <ul>
+                                        <li>{result.nome}</li>
+                                        <li>{result.login}</li>
+                                    </ul>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
             </div>
-            <div className="content" id="content">
-              <div className="content__data">
-                  <div className="content__data--block login">
-                    <label htmlFor="login">Login</label>
-                    <input id="login" type="text" name="login" value=""  />
-                  </div>
-                  <div className="content__data--block email">
-                    <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value=""  />
-                  </div>
-                  <div className="content__data--button">
-                    <label></label>
-                    <button type="submit" className="content__data--button-search">Buscar</button>
-                  </div>
-              </div>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
