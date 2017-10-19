3951cd302f56a3013f118527d80c15e567a6bb98
Diego Marcelo <diego.silva@profite.com.br>
2017-10-19 12:46:45 -0200

    Iniciando estilização da página

--- github-consulta/src/App.js
+++ github-consulta/src/App.js
@@ -13,6 +13,16 @@ class App extends Component {
         return (
             <div id="main">
                 <Header />
+                <div>
+                    <div class="footer_social">
+                      <ul>
+                          <li class="facebook"><a href="https://www.facebook.com/profile.php?id=100008973459426" target="_blank"></a></li>
+                          <li class="linkedin"><a href="https://www.linkedin.com/in/diego-marcelo-02aa147a/" target="_blank"></a></li>
+                          <li class="github"><a href="https://github.com/diegomcsilva" target="_blank"></a></li>
+                      </ul>
+                    </div>
+                </div>
+                <div></div>
                 <div className="content" id="content">
                     <ContentConsult />
                     <div className="content__result">
